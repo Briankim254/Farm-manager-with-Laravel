e3a4a75d573d14006e7ceb5fbcf4b7ee8b7c6e01
Kimuti <kimutaibrian560@gmail.com>
2022-11-18 16:47:45 +0300

    minimal alteration to the cdn resources for jaascript

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -107,9 +107,9 @@
         crossorigin="anonymous"></script>
 <script src="{{asset('js/scripts.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-<script src="{{asset('assets/demo/chart-area-demo.js')}}"></>
+<script src="{{asset('assets/demo/chart-area-demo.js')}}"></script>
 <script src="{{asset('assets/demo/chart-bar-demo.js')}}"></script>
-<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+<script src="{{asset('https://cdn.jsdelivr.net/npm/simple-datatables@latest')}}" crossorigin="anonymous"></script>
 <script src="{{asset('js/datatables-simple-demo.js')}}"></script>
 <script src="{{asset('https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js')}}"></script>
 <link href="{{asset('https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css')}}" rel="stylesheet">
